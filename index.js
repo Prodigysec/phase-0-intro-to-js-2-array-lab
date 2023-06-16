40554db57c0e0a61d93220575e1d1d23665c9bfd
Ian Raburu <williamraburu@gmail.com>
2023-06-16 05:58:03 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+
+// cats.length = 0;
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    cats = [...cats, name];
+    return cats;
+}
+
+function prependCat(name) {
+    cats = [name, ...cats];
+    return cats.slice(0,4);
+    // return cats;
+    
+}
+
+function removeLastCat(name) {
+    return cats.slice(1,3);
+}
+
+function removeFirstCat() {
+    return cats.slice(2,4);
+}
